d6cbb7b2dcfe453c8ac0dab975182fa74154dc19
Izunna-Norbert <agunorbert@gmail.com>
2021-04-02 00:08:51 +0100

    Transction Runner first

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 //Routes middlewares
 app.use('/api', authRoute);
 
-
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log('Server up and running');
   });
